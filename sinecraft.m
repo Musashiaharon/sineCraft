508ddaf423f7e3e5ddff993c1c7a99ebc92fadfb
MusashiAharon <astrochess@gmail.com>
2013-04-25 17:13:33 -0700

    consolidated sine parameters into params variable, added legend to plot

--- sinecraft.m
+++ sinecraft.m
@@ -1,7 +1,8 @@
 fs = 48000;
-len_t = 128;
+len_t = 1024;
+m = 8;
 
-vs = 34030
+vs = 34030; % speed of sound at sea level, in cm/s
 
 fprintf('Computing %g samples. fs is %g.\n', len_t, fs);
 fprintf('Length is %g seconds or\n',len_t/fs);
@@ -10,17 +11,27 @@ fprintf('Distance traversed is %g centimeters.\n',len_t/fs*vs);
 
 t = 0:len_t-1;
 
-len_t = size(t,2);
+  % freq        phase   ampl        offset
+params = [ ...
+    60,         0,      2^14,       2^15        ; ... %1
+    100,        0,      2^14,       2^15        ; ... %2   
+    200,        0,      2^14,       2^15        ; ... %3   
+    440,        0,      2^14,       2^15        ; ... %4
+    880,        0,      2^14,       2^15        ; ... %5   
+    1000,       0,      2^14,       2^15        ; ... %6   
+    5000,       0,      2^14,       2^15        ; ... %7   
+    8000,       0,      2^14,       2^15        ...   %8
+    ];
+    
+f       = params(:,1)';
+phase   = params(:,2)';
+A       = params(:,3)';
+offset  = params(:,4)';
 
-A = [2^14, 2^14, 1.5*2^14, 2^13];
-offset = [2^15, 2^15, 1.5*2^14, 2^15];
-phase = [0,0,0,0];
-f = [440, 3000, 6000, 4000];
+% s = samples
+s = zeros(m,len_t);
 
-
-s = zeros(2,len_t);
-
-for i=1:2
+for i=1:m
     fprintf('\nComputing waveform %d:\n',i);
     fprintf('    f      = %g\n',    f(i));
     fprintf('    A      = %g\n',    A(i));
@@ -31,16 +42,27 @@ end
 
 s=s';
 
-dlmwrite('sines0.txt', dec2bin(s(:,1)),'delimiter','');
-dlmwrite('sines1.txt', dec2bin(s(:,2)),'delimiter','');
+fprintf('\nSaving...\n');
+for i=1:m
+    file = sprintf('%d-%d.txt', f(i), phase(i));
+    dlmwrite(file, s(:,i));
+    fprintf('  Saved %s\n', file);
+end
 
-fprintf('Saved to "sines0.txt" and "sines1.txt".\n');
-fprintf('Plotting...\n');
-
-plot(   t, s(:,1),'b-', ...
-        t, s(:,2),'r-');%, ...
-%        t, s(3,:),'g-', ...
-%        t, s(4,:),'k-');
+fprintf('\nPlotting...\n');
+leg = cell(m,1);
+for i=1:m
+    if i ~= 1 
+        hold all;
+    end
+    plot(t, s(:,i), '-');
+    leg{i} = strcat(        ...
+        num2str(f(i)),      ...
+        'Hz \phi',          ...
+        num2str(phase(i)));
+end
+legend(leg);
+hold off;
 
 fprintf('[Done]\n\n');
 
